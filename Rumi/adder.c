590040203afe50c04b73e8293d33e6947a24e707
Mohammad Muhaiminul Ali Rumi <mohammad.rumi.262@northsouth.edu>
2026-06-22 00:06:30 +0600

    Add basic adder program in C

---
+++ Rumi/adder.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i,j;
+  printf("Hello, Welcome to the Scientific Calculator from CSE 115 Project\n");
+  printf("what you wanna do first??? let's start with the very basic simple operation,yes the adder\nGive your two input\n");
+  scanf("%d\n%d",&i,&j);
+  printf("here's the sweet result %d", i+j);
+}
