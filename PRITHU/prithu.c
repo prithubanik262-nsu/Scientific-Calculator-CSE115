59d664e6c0f0d2ed918c357895debc994c7052d2
Prithu <prithu.banik.262@northsouth.edu>
2026-06-22 20:41:57 +0600

    fully fledged power_root program without the use of imaginary numbers has been coded

--- PRITHU/prithu.c
+++ PRITHU/prithu.c
@@ -8,7 +8,8 @@
 typedef int matrix[max_matrix_size][max_matrix_size];
 
 //function prototypes
-void heading();
+void Welcome_screen();
+void programs_names();
 void power_root();
 void matrices();
 void vector();
@@ -18,7 +19,7 @@ void vector();
 
 int main(void){
     printf("hello world\nthis is the scientific calculator project\n\n");
-
+    Welcome_screen();
     int command;//taking the value for whether the calculator will continue running or just terminate
     int program_option;//this takes the value of option of a program to run
 
@@ -26,7 +27,7 @@ int main(void){
         printf(">>> press 1 to continue or 0 to exit: ");
         scanf("%d", &command);
         if(command == 1){
-            heading();
+            programs_names();
             // printf("\t>>>Choose your program.\n");
             // printf("\t  1.Power_Rootover");
             scanf(" %d", &program_option);
@@ -64,7 +65,7 @@ int main(void){
 }
 
 //this will be printed at the very beginning
-void heading(){
+void Welcome_screen(){
     printf("\t\t ____________________________________ \n");
     printf("\t\t|                                    |\n");
     printf("\t\t|               WELCOME              |\n");
@@ -73,6 +74,8 @@ void heading(){
     printf("\t\t|                                    |\n");
     printf("\t\t|____________________________________|\n");
     printf("\n\n");
+}
+void programs_names(){
     printf("\t>>>The programs are: \n\n");
     printf("\t  1.Power_Rootover\n");
     printf("\t  2.Matrix\n");
@@ -81,8 +84,64 @@ void heading(){
 }
 
 void power_root(){
-    printf("\n\t>>>this is power-root program\n");
+    printf("\n\t>>> This is power-root program\n");
+
+    double a;//this is local variable exclusive to the power_root function
+    int choice;//this variable stores the choice of user of what operation the user wants to use and this variable is used in switch-case
+    double n;//this is for using n-th root and n-th power
+    int rerun_program = 1;//this is variable to control whether the program will rerun or not
+                         //I used a if-statement in the while-loop to update this variable and break out of this loop
+
+    while(rerun_program){
+        if(rerun_program == 1){
+            printf("\n\n\t Press 0 to exit...\n\n");
+            printf("\t >>> Available operations: \n");
+            printf("\t 1. square\n");
+            printf("\t 2. square-root\n");
+            printf("\t 3. n-th power\n");
+            printf("\t 4. n-th root\n");
+
+            printf("\t >>> Choose your operation: ");
+            scanf("%d", &choice);
+
+            if(choice == 0){
+                break;
+            }
+
+            printf("\t >>> Enter number you want to operate on: ");
+            scanf("%lf", &a);
+
+            switch(choice){
+                case 1:
+                    printf("\t The square is: %.2lf", a*a);
+                    break;
+                case 2:
+                    printf("\t The square-root is: %.2lf", sqrt(a));
+                    break;
+                case 3:
+                    printf("\t >> Enter n:");
+                    scanf("%lf", &n);
+                    printf("The %.3lf th power of %.0lf is: ", n, pow(a, n));
+                    break;
+                case 4:
+                    printf("\t >> Enter n:");
+                    scanf("%lf", &n);
+                    printf("The %lf th root of %lf is: ", n, pow(a, 1.0/n));
+                    break;
+                default:
+                    printf("\t\n Invalid Input!");
+                    break;
+
+            }
+        }
+
+        else{
+            break;
+        }
+    }
+
     printf("\n\n");
+
 }
 
 void matrices(){
